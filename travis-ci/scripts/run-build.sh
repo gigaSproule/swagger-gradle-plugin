02daee4b97dcefcdce866a50bdf9272ce8be5207
Benjamin Sproule <benjamin@benjaminsproule.com>
2018-06-01 17:00:51 +0100

    Updated to run different gradle versions depending of java version

--- travis-ci/scripts/run-build.sh
+++ travis-ci/scripts/run-build.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -ev
 ./gradlew clean check
-if [[ "${TRAVIS_JDK_VERSION}" == "openjdk8" || "${TRAVIS_JDK_VERSION}" == "oraclejdk8" || "${TRAVIS_JDK_VERSION}" == "oraclejdk9" ]]; then
-    older_gradle_versions=(3.2 3.5.1 4.0)
-    for gradle_version in ${older_gradle_versions[@]} ; do
-        ./gradlew clean check -Dtest.gradleVersion=${gradle_version}
-    done
+older_gradle_versions=()
+if [[ "${TRAVIS_JDK_VERSION}" == "oraclejdk9" ]]; then
+    older_gradle_versions+=(4.2.1)
+elif [[ "${TRAVIS_JDK_VERSION}" == "openjdk8" || "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ]]; then
+    older_gradle_versions+=(3.2 3.5.1 4.0)
 fi
+for gradle_version in ${older_gradle_versions[@]} ; do
+    ./gradlew clean check -Dtest.gradleVersion=${gradle_version}
+done
 ./gradlew clean install
 ./gradlew -b sample/groovy-spring-boot-jaxrs/build.gradle clean generateSwaggerDocumentation
 ./gradlew -b sample/groovy-spring-boot-mvc/build.gradle clean generateSwaggerDocumentation

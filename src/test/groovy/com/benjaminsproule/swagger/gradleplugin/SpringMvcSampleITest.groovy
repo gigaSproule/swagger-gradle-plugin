3643746c3ed1078134b311c8793a862a5bb045ce
Patrick Coray <patrick.coray@bluewin.ch>
2018-06-22 21:13:00 +0200

    Add default tag when no tags on Api or Operation

--- src/test/groovy/com/benjaminsproule/swagger/gradleplugin/SpringMvcSampleITest.groovy
+++ src/test/groovy/com/benjaminsproule/swagger/gradleplugin/SpringMvcSampleITest.groovy
@@ -66,5 +66,82 @@ class SpringMvcSampleITest extends AbstractPluginITest {
         assert paths."/api/sample".post
         assert paths."/api/sample".post.operationId == "postSample"
     }
- 
+    
+    def 'Produces tag for controller when no Api'() {
+      given:
+      def expectedSwaggerDirectory = "${testProjectOutputDir}/swaggerui-" + UUID.randomUUID()
+      buildFile << """
+            plugins {
+                id 'java'
+                id 'groovy'
+                id 'com.benjaminsproule.swagger'
+            }
+            swagger {
+                apiSource {
+                    springmvc = true
+                    host = 'localhost:8080'
+                    basePath = '/'
+                    info {
+                        title = 'test'
+                        version = '1'
+                    }
+                    swaggerDirectory = '${expectedSwaggerDirectory}'
+                    ${testSpecificConfig}
+                }
+            }
+        """
+       when:
+      def result = runPluginTask()
+       then:
+      result.task(":${GenerateSwaggerDocsTask.TASK_NAME}").outcome == SUCCESS
+       assertSwaggerJsonTags("${expectedSwaggerDirectory}/swagger.json")
+       where:
+      testSpecificConfig << [
+          """
+              locations = ['com.benjaminsproule.swagger.gradleplugin.test.springmvc']
+          """
+      ]
+  }
+  
+  private static void assertSwaggerJsonTags(String swaggerJsonFile) {
+    def producedSwaggerDocument = new JsonSlurper().parse(new File(swaggerJsonFile), 'UTF-8')
+    
+    assert producedSwaggerDocument.swagger == '2.0'
+    assert producedSwaggerDocument.basePath == '/'
+    
+    def info = producedSwaggerDocument.info
+    assert info
+    assert info.version == '1'
+    assert info.title == 'test'
+    
+    def paths = producedSwaggerDocument.get('paths')
+    assert paths
+    assert paths.size() == 4
+    assert paths."/api/sample".get
+    assert paths."/api/sample".get.operationId == "getSample"
+    assert paths."/api/sample".get.tags == ['SampleController']
+    assert paths."/api/sample".post
+    assert paths."/api/sample".post.operationId == "postSample"
+    assert paths."/api/sample".post.tags == ['SampleController']
+    
+    assert paths."/api/other".get
+    assert paths."/api/other".get.operationId == "getOther"
+    assert paths."/api/other".get.tags == ['OtherController']
+    assert paths."/api/other".post
+    assert paths."/api/other".post.operationId == "createOther"
+    assert paths."/api/other".post.tags == ['OtherController']
+    
+    assert paths."/api/pets".get
+    assert paths."/api/pets".get.operationId == "getPets"
+    assert paths."/api/pets".get.tags == ['pets']
+    assert paths."/api/pets/eagles".get
+    assert paths."/api/pets/eagles".get.operationId == "getEagles"
+    assert paths."/api/pets/eagles".get.tags == ['eagles']
+    
+    def tags = producedSwaggerDocument.get('tags')
+    assert tags
+    assert tags.size() == 4
+    assert tags.sort() == [[name:'eagles'], [name:'OtherController'], [name:'pets'], [name:'SampleController']]
+  }
+
 }

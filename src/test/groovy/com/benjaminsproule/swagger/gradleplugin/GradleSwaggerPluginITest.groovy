8f2d0b18310719c2cc83d4bdf9f0ef28f9c3dbef
Mr. No <no@tiger.cx>
2019-03-18 10:09:10 +0100

    no mavenLocal()

--- src/test/groovy/com/benjaminsproule/swagger/gradleplugin/GradleSwaggerPluginITest.groovy
+++ src/test/groovy/com/benjaminsproule/swagger/gradleplugin/GradleSwaggerPluginITest.groovy
@@ -493,7 +493,7 @@ class GradleSwaggerPluginITest extends AbstractPluginITest {
             }
 
             repositories {
-                mavenLocal()
+                // mavenLocal()
                 mavenCentral()
             }
 

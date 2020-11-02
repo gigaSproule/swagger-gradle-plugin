7940b2d3157a5ccc72dfeb6ffcfe04891e1e5c16
wakingrufus <wakingrufus@gmail.com>
2020-11-02 07:49:39 -0600

    Fixes #139 remove early dependency resolution
    resolving the dependencies during configuration is an antipattern in Gradle, https://docs.gradle.org/nightly/userguide/performance.html#dont_resolve_dependencies_at_configuration_time and causes conflicts with other plugins that add dependencies to the project. for example https://github.com/freefair/gradle-plugins/pull/280
    
    parameterize happy path integration test to test against all known supported versions of gradle

---
+++ plugin/src/test/groovy/com/benjaminsproule/swagger/gradleplugin/GradleVersions.groovy
@@ -0,0 +1,9 @@
+package com.benjaminsproule.swagger.gradleplugin
+
+/**
+ * Provides a central place to declare Gradle versions supported by this plugin.
+ * Use this array in parameterized tests to verify against all supported versions.
+ */
+class GradleVersions {
+    public static final String[] SUPPORTED_VERSIONS = ["3.5.1", "4.10.3", "5.6.4", "6.3", "6.7"]
+}

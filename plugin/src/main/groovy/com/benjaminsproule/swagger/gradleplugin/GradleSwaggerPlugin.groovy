9a7482f8e2f41ad03f0d14e7d78544479d9eb661
thorin <117224+thorin@users.noreply.github.com>
2021-07-06 16:43:06 +0100

    delay runtime deps resolution to execution time

--- plugin/src/main/groovy/com/benjaminsproule/swagger/gradleplugin/GradleSwaggerPlugin.groovy
+++ plugin/src/main/groovy/com/benjaminsproule/swagger/gradleplugin/GradleSwaggerPlugin.groovy
@@ -62,7 +62,7 @@ class GradleSwaggerPlugin implements Plugin<Project> {
             }.findAll {
                 it != null
             }
-            generateSwaggerDocsTask.inputFiles = project.configurations.getByName("runtime").files()
+            generateSwaggerDocsTask.inputFiles = project.configurations.getByName("runtime").fileCollection()
         }
     }
 }
